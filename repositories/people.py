fed71255be4213e56b4b4c7b1b50a156c628818d
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-08-16 12:19:10 -0300

    Create People Repository

---
+++ repositories/people.py
@@ -0,0 +1,19 @@
+PEOPLE: map = {
+    "Joao": {
+        "fname": "Joao",
+        "lname": "Silva"
+    },
+    "Maria": {
+        "fname": "Maria",
+        "lname": "Santos"
+    },
+    "Jose": {
+        "fname": "Jose",
+        "lname": "Carvalho"
+    }
+}
+
+
+def find_all():
+    return list(PEOPLE.values())
+
